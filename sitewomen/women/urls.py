d3d37df62c6dad2a3866e215615ca086b3becfc1
AA Karpenko <anastasha.me@gmail.com>
2025-11-13 15:54:29 +0200

    added html pages and their folder templates, also added static for women with folders js, css and images. Added file with css styles and images for site

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -5,10 +5,10 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('about/', views.about, name="about"),
-    path('', views.index, name="home"), # http://127.0.0.1:8000/
-    path('cats/<int:cat_id>/', views.categories, name="cats_id"), # http://127.0.0.1:8000/cats/4/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name="cats"), # http://127.0.0.1:8000/cats/aa/
-    # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive) the same as...
-    path("archive/<year4:year>/", views.archive, name="archive")
+    path("", views.index, name="home"),  # http://127.0.0.1:8000/
+    path("about/", views.about, name="about"),
+    path("addpage/", views.addpage, name="add_page"),
+    path("contact/", views.contact, name="contact"),
+    path("login/", views.login, name="login"),
+    path("post/<int:post_id>/", views.show_post, name="post"),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,38 +1,74 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-
-from django.shortcuts import render, redirect
-from datetime import datetime
+from django.http import (
+    HttpResponse,
+    HttpResponseNotFound,
+    Http404,
+)
+from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.urls import reverse
+
+menu = [
+    {"title": "О сайте", "url_name": "about"},
+    {"title": "Добавить статью", "url_name": "add_page"},
+    {"title": "Обратная связь", "url_name": "contact"},
+    {"title": "Войти", "url_name": "login"},
+]
+
+data_db = [
+    {
+        "id": 1,
+        "title": "Анджелина Джоли",
+        "content": "Биография Анджелины Джоли",
+        "is_published": True,
+    },
+    {
+        "id": 2,
+        "title": "Марго Робби",
+        "content": "Биография Марго Робби",
+        "is_published": False,
+    },
+    {
+        "id": 3,
+        "title": "Джулия Робертс",
+        "content": "Биография Джулии Робертс",
+        "is_published": True,
+    },
+]
+
 
 def index(request):
     # t = render_to_string("woman/index.html")
     # return HttpResponse(t)
-    return render(request, "woman/index.html")
+    data = {"title": "Главная страница", "menu": menu, "posts": data_db}
+
+    return render(request, "women/index.html", context=data)
+
 
 def about(request):
-    return render(request, "woman/about.html")
+    return render(
+        request,
+        "women/about.html",
+        {
+            "title": "О сайте",
+            "menu": menu,
+        },
+    )
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+def addpage(request):
+    return HttpResponse(f"Добавление статьи")
 
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        res = []
-        for key, value in request.GET.items():
-            res.append(f"{key}={value}")
-        return HttpResponse("|".join(res))
-    else:
-        return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
 
-def archive(request, year):
-    if year > datetime.now().year:
-        uri = reverse(categories_by_slug, args=("music", ))
-        return redirect(uri)
-        # return HttpResponsePermanentRedirect(uri)
-    else:
-        return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def contact(request):
+    return HttpResponse(f"Обратная связь")
+
+
+def login(request):
+    return HttpResponse(f"Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
