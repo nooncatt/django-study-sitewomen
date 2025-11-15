857eb7974896cb2001466d83a59595a4bc377ec9
AA Karpenko <anastasha.me@gmail.com>
2025-11-15 18:50:58 +0200

    extended templates, using simple and inclusion_tag, added some new html files for it and directories as templatetags. Also organaised static files. Added my first class Women in models in order to use sqlite.db

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -17,7 +17,8 @@ data_db = [
     {
         "id": 1,
         "title": "Анджелина Джоли",
-        "content": "Биография Анджелины Джоли",
+        "content": """<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+        Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».""",
         "is_published": True,
     },
     {
@@ -29,16 +30,24 @@ data_db = [
     {
         "id": 3,
         "title": "Джулия Робертс",
-        "content": "Биография Джулии Робертс",
+        "content": "Биография Джулия Робертс",
         "is_published": True,
     },
+]
+cats_db = [
+    {"id": 1, "name": "Актрисы"},
+    {"id": 2, "name": "Певицы"},
+    {"id": 3, "name": "Спортсменки"},
 ]
 
 
 def index(request):
-    # t = render_to_string("woman/index.html")
-    # return HttpResponse(t)
-    data = {"title": "Главная страница", "menu": menu, "posts": data_db}
+    data = {
+        "title": "Главная страница",
+        "menu": menu,
+        "posts": data_db,
+        "cat_selected": 0,
+    }
 
     return render(request, "women/index.html", context=data)
 
@@ -68,6 +77,17 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    data = {
+        "title": "Отображение по рубрикам",
+        "menu": menu,
+        "posts": data_db,
+        "cat_selected": cat_id,
+    }
+
+    return render(request, "women/index.html", context=data)
 
 
 def page_not_found(request, exception):
